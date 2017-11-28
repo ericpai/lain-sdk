b3cb282e5dc9d36c625d1d22247832bfc9e54c1d
zc <greysavior@foxmail.com>
2017-11-28 17:23:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
     'enum34==1.0.4',
     'requests>=2.6.1',
     'websocket-client==0.32.0',
-    'docker==2.6.1',
+    'docker==2.6.0',
     'pytest<=3.2.5',
     'mock==1.0.1',
     'jsonschema==2.5.1',
